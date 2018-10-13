309d03d4aa5b6cbe008de96a822f56357e423019
kirill-mac <k.fedoseev@innopolis.ru>
2018-10-13 15:52:14 +0300

    Some changes

---
+++ GetAccessRights.sql
@@ -0,0 +1,16 @@
+CREATE PROCEDURE `GetAccessRights` (login varchar(30), passHash varchar(25) , out access varchar(45))
+BEGIN
+
+    declare a int;
+    
+	Select e.AccessRights_idAccessRights
+	from Employees  as e 
+	where e.Login == login and e.PassHash == passHash
+	into a;
+    
+    select access_role from AccessRights as r 
+    where a ==  r.idAccessRights
+    into access;
+    
+
+END
