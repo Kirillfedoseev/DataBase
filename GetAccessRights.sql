836b3de545b452b008bef24f8c0ab7528252b2f2
Kirillfedoseev <k.fedoseev@innopolis.ru>
2018-10-13 17:15:51 +0300

    no message

--- GetAccessRights.sql
+++ GetAccessRights.sql
@@ -1,16 +1,15 @@
-CREATE PROCEDURE `GetAccessRights` (login varchar(30), passHash varchar(25) , out access varchar(45))
+CREATE DEFINER=`root`@`%` PROCEDURE `GetAccessRights`(login varchar(30), passHash varchar(25) , out access varchar(45))
 BEGIN
-
-    declare a int;
+    
+    declare a int default '-1';
     
 	Select e.AccessRights_idAccessRights
 	from Employees  as e 
-	where e.Login == login and e.PassHash == passHash
+	where e.Login = login and e.PassHash = passHash
 	into a;
     
     select access_role from AccessRights as r 
-    where a ==  r.idAccessRights
+    where a =  r.idAccessRights
     into access;
-    
 
 END
